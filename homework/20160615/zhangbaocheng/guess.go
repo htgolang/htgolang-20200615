beecc3733d23df5fa37b3b7cea55f89ea661f5c7
zhangbaocheng <13725578065@163.com>
2019-06-17 17:54:51 +0800

    day1 作业

---
+++ homework/20160615/zhangbaocheng/guess.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+START:
+	rand.Seed(time.Now().Unix())
+	num := rand.Int() % 101
+	fmt.Println(num)
+	for j:=5; j >=1; j--{
+		var input_num int
+		fmt.Print("请输入一个0-100的整数:")
+		fmt.Scan(&input_num)
+		if input_num  == num  {
+			fmt.Println("恭喜你，你猜对了！你是我肚子里的蛔虫吗？")
+			goto START
+		}else if input_num > num {
+			if j==1{
+				fmt.Println("你太笨了，这都没猜到, 游戏重新开始 ！")
+				goto START
+			}else {
+				fmt.Println("大啦，大啦，大啦")
+			}
+
+		}else if input_num < num {
+			if j==1 {
+				fmt.Println("你太笨了，这都没猜到,游戏重新开始 ！")
+				goto START
+			}else {
+				fmt.Println("小啦，小啦，小啦")
+
+			}
+
+		}
+	}
+
+
+}
