a36fd21379c50f15dfc2ca2996440043972b897c
Aegwyn <xxdu521@163.com>
2019-06-18 14:21:28 +0800

    first commit

---
+++ homework/20160615/daa/001_1.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("打印乘法口诀表：")
+	for d1 := 1; d1 <= 9; d1++ {
+		for d2 := 1; d2 <= 9; d2++ {
+			if d2 > d1 {
+				break
+			}
+			fmt.Printf("%d X %d = %2d  ", d2, d1, d1*d2)
+		}
+		fmt.Println()
+	}
+}
---
+++ homework/20160615/daa/001_2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	//提前声明变量，防止循环声明引起异常
+	var i int
+	var ReadNum int
+
+	for {
+		//逻辑锁在死循环里
+		//程序起点，简称打开游戏，以下都为反复循环角度考虑
+	STARTEND:
+		//重置随机数
+		rand.Seed(time.Now().Unix())
+		RandNum := rand.Int() % 100
+		//打印出随机数，方便测试数字大小的提示。
+		//fmt.Println(RandNum)
+
+		//开始猜数主体
+		for i = 5; i >= 0; i-- {
+			//调试i的值
+			//fmt.Println(i)
+			//判断游戏何时退出
+			if i != 0 {
+				//开始正常游戏过程
+				fmt.Printf("请猜数字，你还有%d次机会，加油！！: ", i)
+				fmt.Scan(&ReadNum)
+				switch {
+				case ReadNum > RandNum:
+					fmt.Println("有点大了")
+				case ReadNum < RandNum:
+					fmt.Println("有点小了")
+				case ReadNum == RandNum:
+					fmt.Println("恭喜你，猜中了！！")
+					fmt.Println("请重新开始猜数游戏。。")
+					//重新打开游戏
+					goto STARTEND
+				}
+			} else {
+				fmt.Println("你太笨了")
+				//重新打开游戏
+				goto STARTEND
+
+			}
+
+		}
+
+	}
+}
