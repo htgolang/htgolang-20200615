93adc25209f5003f23b15acab2ceec3b6b2e0781
WPFS <823431828@qq.com>
2019-06-26 15:39:08 +0800

    第二次task

---
+++ homework/20190622/wupengfei/mapvalue.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var maptask map[string]string = map[string]string{"name": "wpfs", "age": "18"}
+	mapslice := []string{}
+	for _, v := range maptask {
+		mapslice = append(mapslice, v)
+	}
+	fmt.Println(mapslice)
+}
