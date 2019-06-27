56f75936c3d66b6550ed8fd4cbb3a277dffe8075
dongranliu <hysg95324@gmail.com>
2019-06-27 17:30:43 +0800

    提交作业

--- homework/20190622/liudongran/homework.go
+++ homework/20190622/liudongran/homework.go
@@ -34,5 +34,38 @@ func main() {
 		values = append(values, v)
 	}
 	//  打印映射中所有key组成的切片和打印映射中所有value组成的切片
-	fmt.Printf("keys组成的切片:%v\nvalues组成的切片:%v\n",keys, values)
+	fmt.Printf("keys组成的切片:%v\nvalues组成的切片:%v\n", keys, values)
+
+	wordCount01 := map[rune]int{}
+	wordCount02 := map[int][]rune{}
+
+	for _, word := range article {
+		if word >= 'A' && word <= 'Z' || word >= 'a' && word <= 'z' {
+			wordCount01[word]++
+		}
+	}
+
+	for key, value := range wordCount01 {
+		if v, ok := wordCount02[value]; !ok {
+			wordCount02[value] = []rune{key}
+		} else {
+			v = append(v, key)
+		}
+	}
+	fmt.Print("统计出现特定次数的单词：")
+	for count, value := range wordCount02 {
+		fmt.Printf("%d:%c, ", count, value)
+	}
+	fmt.Println()
+
+	// 7. 冒泡排序，这里使用第一题的切片 ints
+	n := len(ints) - 1
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i; j++ {
+			if ints[j] > ints[j+1] {
+				ints[j], ints[j+1] = ints[j+1], ints[j]
+			}
+		}
+	}
+	fmt.Printf("冒泡排序：%v", ints)
 }
