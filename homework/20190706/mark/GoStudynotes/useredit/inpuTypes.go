648b037d45985c5368d481195a30911c0d168347
mark <usertzc@gmail.com>
2019-07-12 22:03:52 +0800

    mark

--- homework/20190706/mark/GoStudynotes/useredit/inpuTypes.go
+++ homework/20190706/mark/GoStudynotes/useredit/inpuTypes.go
@@ -52,29 +52,28 @@ func InputYMD(prompt ...string)(string){
 			fmt.Printf("[err]请重新输入，并确定你输入的是一个年份!如:[%v]\n", time.Now().Year())
 			continue
 		} else if year >= time.Now().Year() || yearlen != 4 {
-			fmt.Printf("[err]年份错误，请重新输入年份!不能大于%v\n",time.Now().Year())
+			fmt.Printf("[err]年份错误，请重新输入年份且不能大于%v\n",time.Now().Year())
 			continue
 		}
 		for {
 			fmt.Print("请输入月份:")
 			fmt.Scan(&month)
-			monthlen := len(month)
+			//monthlen := len(month)
 			if month, err := strconv.Atoi(month); err != nil {
-				fmt.Print("[err]请重新输入，并确定你输入的是一个月份!如:[12]")
+				fmt.Print("[err]请重新输入，并确定你输入的是一个月份!如:[12]\n")
 				continue
-			} else if monthlen > 2 || month >= 13 {
+			} else if  month >= 13 {
 				fmt.Print("[err]月份错误，请重新输入月份!不能大于12月\n")
 				continue
 			}
 			for {
 				fmt.Print("请输入日期:")
 				fmt.Scan(&day)
-				daylen := len(day)
 				if day,err := strconv.Atoi(day);err != nil {
-					fmt.Print("[err]请重新输入，并确定你输入是一个日期!如:[31]")
+					fmt.Print("[err]请重新输入，并确定你输入是一个日期!如:[31]\n")
 					continue
-				}else if daylen >= 3 || day >= 32{
-					fmt.Print("[err]日期错误，请重新输入日期!不能大于%v号\n")
+				}else if day >= 32{
+					fmt.Print("[err]日期错误，请重新输入日期!不能大于31号\n")
 					continue
 				}
 				break
