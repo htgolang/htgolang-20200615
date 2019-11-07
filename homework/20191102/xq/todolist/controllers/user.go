9f6ea619e0375918caa7d5beb15eacca38b06f33
xq <xiaoqiang@xiaoqiangdeMacBook-Pro.local>
2019-11-07 23:38:43 +0800

    20191107

--- homework/20191102/xq/todolist/controllers/user.go
+++ homework/20191102/xq/todolist/controllers/user.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+
 	"net/http"
 	"strings"
 	"time"
@@ -85,7 +86,7 @@ func (c *UserController) Create() {
 		form := &forms.UserCreateForm{} // 用户创建表单
 		valid := &validation.Validation{} //验证器
 
-	//if c.Ctx.Input.IsPost() {
+
 		// 解析请求参数到form中(根据form标签)
 		if err := c.ParseForm(form); err != nil {
 			json["text"] = err.Error()
@@ -148,32 +149,63 @@ func (c *UserController) Create() {
 
 // 用户修改
 func (c *UserController) Modify() {
+
+	json := map[string]interface{}{
+		"code": 405,
+		"text": "请求方式错误",
+		"result": nil,
+	}
+
 	form := &forms.UserModifyForm{} // 修改表单
 	valid := &validation.Validation{} //验证器
+
+
 	if c.Ctx.Input.IsGet() {
+
 		// 获取用户信息(显示)
-		if id, err := c.GetInt("id"); err == nil {
+		if id, err := c.GetInt("id"); err != nil {
+			json["code"] = 400
+			json["text"] = "获取ID失败"
+			json["result"] = err.Error()
+
+		}else{
+
 			user := models.User{Id: id}
-			if orm.NewOrm().Read(&user) == nil {
-				form.Id = user.Id
-				form.Name = user.Name
-				if user.Sex {
-					form.Sex = 1
-				}
-				if user.Birthday != nil {
-					form.Birthday = user.Birthday.Format("2006-01-02")
-				}
-				form.Addr = user.Addr
-				form.Tel = user.Tel
-				form.Desc = user.Desc
+
+			if err := orm.NewOrm().Read(&user);err != nil {
+				json["code"] = 400
+				json["text"] = "获取数据失败"
+				json["result"] = err.Error()
+
+			}else {
+
+				json["code"] = 200
+				json["text"] = "获取数据成功"
+				json["result"] = user
 			}
 
 		}
+
+		c.Data["json"] = json
+		c.ServeJSON()
+
 	} else if c.Ctx.Input.IsPost() {
 		// 解析请求参数到form中(根据form标签)
-		if c.ParseForm(form) == nil {
+		if err := c.ParseForm(form); err != nil {
+			json["code"] = 500
+			json["text"] = "解析失败"
+			json["result"] = err.Error()
+
+		}else {
 			// 验证表单数据
-			if corret, err := valid.Valid(form); err == nil && corret {
+			if corret, err := valid.Valid(form); err != nil {
+				json["code"] = 400
+				json["result"] = err.Error()
+
+			}else if !corret {
+				json["code"] = 400
+
+			}else {
 
 				// 更新用户信息
 				birthday, _ := time.Parse("2006-01-02", form.Birthday)
@@ -185,51 +217,96 @@ func (c *UserController) Modify() {
 				form.User.Addr = form.Addr
 				form.User.Desc = form.Desc
 
-				// 更新数据库
-				ormer := orm.NewOrm()
-				ormer.Update(form.User)
 
-				// 通过flash提示用户操作结果
-				flash := beego.NewFlash()
-				flash.Success("修改用户成功")
-				flash.Store(&c.Controller)
-				c.Redirect(beego.URLFor("UserController.Index"), http.StatusFound)
+				if _, err := orm.NewOrm().Update(form.User); err == nil {
+					json["code"] = 200
+					json["text"] = "修改成功"
+					json["result"] = form.User
+
+				}else {
+
+					json["code"] = 500
+					json["text"] = "服务器错误"
+					json["result"] = nil
+				}
+
 			}
 		}
 	}
 
-	c.TplName = "user/modify.html"
-	c.Data["xsrf_token"] = c.XSRFToken() // 生成csrftoken
-	c.Data["form"] = form
-	c.Data["validation"] = valid
+	c.Data["json"] = json
+	c.ServeJSON()
+
 }
 
 // 用户删除
 func (c *UserController) Delete() {
-	if id, err := c.GetInt("id"); err == nil {
-		// 删除用户
-		orm.NewOrm().Delete(&models.User{Id: id})
 
-		// 通过flash提示用户操作结果
-		flash := beego.NewFlash()
-		flash.Success("删除用户成功")
-		flash.Store(&c.Controller)
+	json := map[string]interface{}{
+		"code":   405,
+		"text":   "请求方式错误",
+		"result": nil,
 	}
-	c.Redirect(beego.URLFor("UserController.Index"), http.StatusFound)
-}
+	if c.Ctx.Input.IsPost() {
+		if id, err := c.GetInt("id"); err != nil {
 
+			json["code"] = 400
+			json["text"] = "获取ID失败"
+			json["result"] = err.Error()
+
+		}else{
+			// 删除用户
+			orm.NewOrm().Delete(&models.User{Id: id})
+
+			// 通过flash提示用户操作结果
+			json["code"] = 200
+			json["text"] = "删除成功"
+			json["result"] = nil
+
+		}
+
+		}
+
+		c.Data["json"] = json
+		c.ServeJSON()
+	}
 
 // 修改密码
 func (c *UserController) Password() {
+
+	json := map[string]interface{}{
+		"code":   405,
+		"text":   "请求方式错误",
+		"result": nil,
+	}
+
 	form := &forms.ModifyPasswordForm{User: c.User} //修改密码表单
 	valid := &validation.Validation{} // 验证器
+
 	if c.Ctx.Input.IsPost() {
 
 		// 解析请求参数到form中(根据form标签)
-		if c.ParseForm(form) == nil {
+		if err := c.ParseForm(form) ; err != nil {
+
+			json["code"] = 400
+			json["text"] = "解析数据错误"
+			json["result"] = err.Error()
+
+		}else {
 
 			// 表单验证
-			if corret, err := valid.Valid(form); err == nil && corret {
+			if corret, err := valid.Valid(form); err != nil {
+				json["code"] = 400
+				json["text"] = "表单验证失败"
+				json["result"] = err.Error()
+
+
+			}else if !corret {
+
+				json["code"] = 400
+				json["result"] = nil
+
+			}else{
 
 				// 设置密码
 				c.User.SetPassword(form.NewPassword)
@@ -239,36 +316,57 @@ func (c *UserController) Password() {
 				// 只更新密码列
 				ormer.Update(c.User, "Password")
 
-				// 通过flash通知用户操作结果(注意，此处只使用flash对象，并未调用store到cookie中)
-				flash := beego.NewFlash()
-				flash.Success("修改密码成功")
-				c.Data["flash"] = flash.Data
+				json["code"] = 200
+				json["text"] = "修改密码成功"
+				json["result"] = nil
+
+
 			}
 		}
 	}
-	c.TplName = "user/password.html"
-	c.Data["xsrf_token"] = c.XSRFToken() //生成csrftoken
-	c.Data["form"] = form
-	c.Data["validation"] = valid
+	c.Data["json"] = json
+	c.ServeJSON()
 }
 
 //重置密码
 func (c *UserController) ResetPassword() {
-	if id, err := c.GetInt("id"); err == nil && c.User.IsSuper {
 
-		user := models.User{Id: id}
-		ormer := orm.NewOrm()
-		if err := ormer.Read(&user); err == nil {
-			password := utils.RandString(6) // 生成随机6位密码
-			user.SetPassword(password) // 设置密码
-			ormer.Update(&user, "Password") //更新数据库
-
-			// 通过flash通知用户重置成功和重置后密码
-			flash := beego.NewFlash()
-			flash.Success("重置用户<%s>密码为<%s>", user.Name, password)
-			flash.Store(&c.Controller)
-
-		}
+	json := map[string]interface{}{
+		"code":   405,
+		"text":   "请求方式错误",
+		"result": nil,
 	}
-	c.Redirect(beego.URLFor("UserController.Index"), http.StatusFound)
+
+	if c.Ctx.Input.IsPost(){
+
+		if id, err := c.GetInt("id"); err != nil{
+
+			json["code"] = 400
+			json["text"] = "获取ID失败"
+			json["result"] = err.Error()
+
+		} else if !c.User.IsSuper {
+			json["code"] = 405
+			json["text"] = "没有权限"
+			json["result"] = err.Error()
+
+		}else {
+
+			user := models.User{Id: id}
+			ormer := orm.NewOrm()
+			if err := ormer.Read(&user); err == nil {
+				password := utils.RandString(6) // 生成随机6位密码
+				user.SetPassword(password) // 设置密码
+				ormer.Update(&user, "Password") //更新数据库
+
+				json["code"] = 200
+				json["text"] = "重置密码成功"
+				json["result"] = password
+
+			}
+		}
+
+	}
+	c.Data["json"] = json
+	c.ServeJSON()
 }
